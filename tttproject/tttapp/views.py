6ffb0b94062d715742790010fb8434bb71709f2b
Asheesh Laroia <asheesh@sandstorm.io>
2017-01-18 01:25:09 -0800

    Add GET param validation

--- tttproject/tttapp/views.py
+++ tttproject/tttapp/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from . import gamelogic
 
-# Create your views here.
 def home(request):
+    board = request.GET.get('board', '')
+    if (not gamelogic.is_playable_by_o(board) or
+        not gamelogic.board_is_valid(board)
+    ):
+        return HttpResponse('Board invalid', status=400)
     return HttpResponse('ok')
 
 def selftest(request):
