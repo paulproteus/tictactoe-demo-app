4f91327c973695abcffed0309c15af4a1ffa85e5
Asheesh Laroia <asheesh@sandstorm.io>
2017-01-18 01:00:38 -0800

    Add trivial board validation

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 env/bin/flake8 tttproject --exclude=tttproject/manage.py,tttproject/tttproject/settings.py
+source env/bin/python
+cd tttproject ; python manage.py test tttapp
--- tttproject/tttapp/tests.py
+++ tttproject/tttapp/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from tttapp.gamelogic import TicTacToeBoard
 
-# Create your tests here.
+class GameLogicTests(TestCase):
+    def test_invalid_boards(self):
+        for invalid_board in [
+                '',
+                (' ' * 10),
+                'xxxxxxxxz',
+        ]:
+            self.assertFalse(TicTacToeBoard.is_valid(invalid_board))
+
+    def test_valid_boards(self):
+        for valid_board in [
+                'x' + ' ' * 8,
+                'xoxxoxxox',
+        ]:
+            self.assertTrue(TicTacToeBoard.is_valid(valid_board))
+
+
+    #def test_find_o_winner(self):
+    #a    board = TicTacToeBoard('ooo x x x')
+    #    self.assertEqual(board.get_shallow_value(), 1)
