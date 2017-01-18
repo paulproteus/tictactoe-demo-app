927e948ff6b7a8bc44ae69b99280207c4c9a45ad
Asheesh Laroia <asheesh@sandstorm.io>
2017-01-18 01:11:39 -0800

    Fix script error

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 env/bin/flake8 tttproject --exclude=tttproject/manage.py,tttproject/tttproject/settings.py
-source env/bin/python
+source env/bin/activate
 cd tttproject ; python manage.py test tttapp
